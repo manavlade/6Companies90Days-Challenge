7a42a19f29be6fba9568644d7ceed5a3645ca2e3
manavlade <manavlade14690@gmail.com>
2024-01-07 00:20:13 +0530

    updated

---
+++ 6Companies90-Days/Microsoft/Question2.java
@@ -0,0 +1,26 @@
+/*
+ Question 2
+ There are n friends that are playing a game. The friends are sitting in a circle and are numbered from 1 to n in clockwise order. 
+ More formally, moving clockwise from the ith friend brings you to the (i+1)th friend for 1 <= i < n, 
+ and moving clockwise from the nth friend brings you to the 1st friend.
+The rules of the game are as follows: Start at the 1st friend.
+Count the next k friends in the clockwise direction including the friend you started at. The counting wraps around the circle and may count some friends more than once.
+The last friend you counted leaves the circle and loses the game.
+If there is still more than one friend in the circle, go back to step 2 starting from the friend immediately clockwise of the friend who just lost and repeat.
+Else, the last friend in the circle wins the game.
+Given the number of friends, n, and an integer k, return the winner of the game.
+
+ 
+ */
+public class Question2 {
+    public int findTheWinner(int n, int k) {
+        int winner = solution(n, k) + 1;
+        return winner;
+    }
+    public int solution(int n, int k){
+        if(n ==1){
+            return 0;
+        }
+        return (solution(n-1, k) + k) % n;
+    }
+}
