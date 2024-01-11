fe6cdd1c7b6865283880f4eca433dbd16191ca83
manavlade <manavlade14690@gmail.com>
2024-01-11 15:29:19 +0530

    Started question 1 of walmart

---
+++ 6Companies90-Days/Walmart/Question1.java
@@ -0,0 +1,43 @@
+/*
+ Question 1
+ Given a set of distinct positive integers nums, return the largest subset answer
+  such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
+answer[i] % answer[j] == 0, or
+answer[j] % answer[i] == 0
+If there are multiple solutions, return any of them.
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Question1 {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        int[] count = new int[n];
+        int[] pre = new int[n];
+        Arrays.sort(nums);
+        int max = 0, index = -1;
+        for (int i = 0; i < n; i++) {
+            count[i] = 1;
+            pre[i] = -1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] % nums[j] == 0) {
+                    if (1 + count[j] > count[i]) {
+                        count[i] = count[j] + 1;
+                        pre[i] = j;
+                    }
+                }
+            }
+            if (count[i] > max) {
+                max = count[i];
+                index = i;
+            }
+        }
+        List<Integer> divisible = new ArrayList<>();
+        while (index != -1) {
+            divisible.add(nums[index]);
+            index = pre[index];
+        }
+        return divisible;
+    }
+}
