4a06cf60af62004f8ddf6758980fc6a2dec00aa4
manavlade <manavlade14690@gmail.com>
2024-01-15 23:57:28 +0530

    added question 3 and 12

--- 6Companies90-Days/Walmart/Question3.java
+++ 6Companies90-Days/Walmart/Question3.java
@@ -1,8 +1,27 @@
-import java.util.Deque;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+/*
+ Question 3 
+ Given two integer arrays nums1 and nums2, 
+ return the maximum length of a subarray that appears in both arrays.
+ */
+
 
 public class Question3 {
-    // Question way too dificult working on it
+    public int findLength(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int [][] dp = new int[m+1][n+1];
+        int maxlength = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    maxlength = Math.max(maxlength, dp[i][j]);
+                }
+            }
+        }
+        return maxlength;
+    }
+
 }
