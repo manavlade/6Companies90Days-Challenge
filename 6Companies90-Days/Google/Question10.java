8abb42418baf1736b6a7324f42705e40e0c771ec
manavlade <manavlade14690@gmail.com>
2024-01-30 06:16:31 +0530

    Added question 10

---
+++ 6Companies90-Days/Google/Question10.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+/*
+ Question 10
+You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents
+the width and the height of an envelope. One envelope can fit into another if and only
+if both the width and height of one envelope are greater than the other envelope's width and height.
+Return the maximum number of envelopes you can Russian doll (i.e., put one inside the other).
+Note: You cannot rotate an envelope.
+ */
+public class Question10 {
+    
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> { return a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]; });
+        int n = envelopes.length;
+        int[] d = new int[n + 1];
+        d[1] = envelopes[0][1];
+        int size = 1;
+        for (int i = 1; i < n; ++i) {
+            int x = envelopes[i][1];
+            if (x > d[size]) {
+                d[++size] = x;
+            } else {
+                int left = 1, right = size;
+                while (left < right) {
+                    int mid = (left + right) >> 1;
+                    if (d[mid] >= x) {
+                        right = mid;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+                int p = d[left] >= x ? left : 1;
+                d[p] = x;
+            }
+        }
+        return size;
+    }
+}
